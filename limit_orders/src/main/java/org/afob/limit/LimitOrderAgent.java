03a6cc0522919faafd10b33da77a664f43cba653
Veera <chakradharchandrala@gmail.com>
2024-03-01 11:08:59 +0000

    Update file LimitOrderAgent.java

--- limit_orders/src/main/java/org/afob/limit/LimitOrderAgent.java
+++ limit_orders/src/main/java/org/afob/limit/LimitOrderAgent.java
@@ -7,13 +7,35 @@ import java.math.BigDecimal;
 
 public class LimitOrderAgent implements PriceListener {
 
-    public LimitOrderAgent(final ExecutionClient ec) {
+    public ExecutionClient ec;
+    private final List<Orders> orders;
+
+    public LimitOrderAgent(final ExecutionClient ec, List<Orders> orders) {
+        this.orders = orders;
+		this.ec = ec;
     }
 
 
     @Override
     public void priceTick(String productId, BigDecimal price) {
 
+        BigDecimal bg = new BigDecimal("100");
+    	
+    	if(productId == "IBM" && price.compareTo(bg) < 1) {
+    		try {
+				ec.buy("IBM", 1000);
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+    	}
+
     }
+
+    public void addOrder(String actionFlag, String productId, int amount, BigDecimal limit) {
+
+			 Orders order = new Orders(actionFlag, productId, amount, limit);
+			 orders.add(order);
+             orders.stream().map(o->{if(o.actionFlag == sell && limit.compareTo(PriceListener.priceTick(o.productId))<1){ec.sell(o.productId,o.amount)} else if(o.actionFlag == buy && limit.compareTo(PriceListener.priceTick(o.productId))<1){ec.buy(o.productId,o.amount)} });
+	}
 
 }
